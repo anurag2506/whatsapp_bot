308e2e203538a8dbbcbd93df71ffbadf85564258
anurag2506 <anuraldo@gmail.com>
2025-02-14 17:08:27 +0530

    setting up the db

--- database.py
+++ database.py
@@ -1,18 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
+import openai
 
 DB_NAME = "expense_tracker_bot"
 DB_USER = "anuragprasad"
 DB_PASSWORD = "2506"
 DB_HOST = "localhost"
-DB_PORT = "5432" #default port of postgres
+DB_PORT = "5432"  # Default port of PostgreSQL
 
 def get_db_connection():
+    """Establishes a PostgreSQL database connection."""
     try:
         DATABASE_URL = f'postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
         engine = create_engine(DATABASE_URL)
         connection = engine.raw_connection()
         return connection
     except SQLAlchemyError as e:
+        print(f"Database Connection Error: {e}")
         return None
-
